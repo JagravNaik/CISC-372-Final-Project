b76d86b909bfb74d2ff9264b96c38cb4ed8e58e1
Jagrav Naik <jagravnaik0@gmail.com>
2021-11-30 22:12:41 -0500

    Create the matrices to multiply
    Signed-off-by: Jagrav Naik <jagravnaik0@gmail.com>

--- serial_matrix.c
+++ serial_matrix.c
@@ -41,52 +41,63 @@ void mulMat(int mat1[][C1], int mat2[][C2]) {
 	}
 }
 
-int main(void) {
-	// Square Matrices
-	// R1 = 4, C1 = 4 and R2 = 4, C2 = 4 (Update these values in MACROs)
-	int mat1[R1][C1] = {
-			{1, 1, 1, 1},
-			{2, 2, 2, 2},
-			{3, 3, 3, 3},
-			{4, 4, 4, 4}
-	};
-
-	int mat2[R2][C2] = {
-			{1, 1, 1, 1},
-			{2, 2, 2, 2},
-			{3, 3, 3, 3},
-			{4, 4, 4, 4}
-	};
-
-	/*
-	// Rectangular Matrices
-	// R1 = 3, C1 = 4 and R2 = 4, C2 = 3 (Update these values in MACROs)
-	int mat1[R1][C1] = {
-			{1, 1, 1, 1},
-			{2, 2, 2, 2},
-			{3, 3, 3, 3}
-	};
-
-	int mat2[R2][C2] = {
-			{1, 1, 1},
-			{2, 2, 2},
-			{3, 3, 3},
-			{4, 4, 4}
-	};
-	*/
-
-	if (C1 != R2) {
-		printf("The number of columns in Matrix-1 must be equal to the number of rows in "
-				"Matrix-2\n");
-		printf("Please update MACROs value according to your array dimension in "
-				"#define section\n");
-
-		exit(EXIT_FAILURE);
+void fillMatrix(int min, int max, int size, float *mat) {
+	srand(time(NULL));
+	for (int i = 0; i < mat; i++) {
+		mat[i] = (rand() % (max - min + 1)) + min;
 	}
-
-	mulMat(mat1, mat2);
-
-	return 0;
 }
+
+int main(void) {
+
+int num_rows_A = 900; int num_rows_B = 900; int num_rows_C = 900;
+int num_cols_A = 900; int num_cols_B = 600; int num_cols_C = 600;
+
+// 	// Square Matrices
+// 	// R1 = 4, C1 = 4 and R2 = 4, C2 = 4 (Update these values in MACROs)
+// 	/*int mat1[R1][C1] = {
+// 			{1, 1, 1, 1},
+// 			{2, 2, 2, 2},
+// 			{3, 3, 3, 3},
+// 			{4, 4, 4, 4}
+// 	};
+
+// 	int mat2[R2][C2] = {
+// 			{1, 1, 1, 1},
+// 			{2, 2, 2, 2},
+// 			{3, 3, 3, 3},
+// 			{4, 4, 4, 4}
+// 	};
+
+// 	/*
+// 	// Rectangular Matrices
+// 	// R1 = 3, C1 = 4 and R2 = 4, C2 = 3 (Update these values in MACROs)
+// 	int mat1[R1][C1] = {
+// 			{1, 1, 1, 1},
+// 			{2, 2, 2, 2},
+// 			{3, 3, 3, 3}
+// 	};
+
+// 	int mat2[R2][C2] = {
+// 			{1, 1, 1},
+// 			{2, 2, 2},
+// 			{3, 3, 3},
+// 			{4, 4, 4}
+// 	};
+// 	*/
+
+// 	if (C1 != R2) {
+// 		printf("The number of columns in Matrix-1 must be equal to the number of rows in "
+// 				"Matrix-2\n");
+// 		printf("Please update MACROs value according to your array dimension in "
+// 				"#define section\n");
+
+// 		exit(EXIT_FAILURE);
+// 	}
+
+// 	mulMat(mat1, mat2);
+
+// 	return 0;
+// }
 
 // This code is contributed by Manish Kumar (mkumar2789)
